d0d76d8470e1fa7835164bb1bde1d19f7c266360
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-25 09:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your code here
+let draggedItem = null;
+
+function dragStart(event) {
+    draggedItem = event.target;
+    event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+function dragEnter(event) {
+    event.preventDefault();
+}
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    const droppedItem = event.target;
+    if (droppedItem.tagName === 'DIV') {
+        event.preventDefault();
+        const draggedItemId = draggedItem.id;
+        const droppedItemId = droppedItem.id;
+        
+        // Swap background images
+        const tempBackground = draggedItem.style.backgroundImage;
+        draggedItem.style.backgroundImage = droppedItem.style.backgroundImage;
+        droppedItem.style.backgroundImage = tempBackground;
+        
+        // Swap IDs to maintain correct drag behavior
+        draggedItem.id = droppedItemId;
+        droppedItem.id = draggedItemId;
+    }
+}
+
+const divs = document.querySelectorAll('[draggable="true"]');
+divs.forEach(div => {
+    div.addEventListener('dragenter', dragEnter);
+    div.addEventListener('dragover', dragOver);
+    div.addEventListener('drop', drop);
+});
